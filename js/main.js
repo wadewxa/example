256b4f88bd73ee73f7d9aa769d156b9fadb2253d
wadewxa <wade.wxa@gmail.com>
2017-07-13 13:25:28 +0800

    First initialized.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+new WoW().init();
+//$(function () {
+//    $('nav a').bind('click', function () {
+//        var $anchor = $(this);
+//        var navHeight = 50;
+//        var time = 1500;
+//        var timingFunction = 'easeInOutCubic';
+//        var target = $anchor.attr('href');
+//
+//        $('html body').stop().animate({
+//            scrollTop: ($(target).offset().top)
+//        }, time, timingFunction);
+//
+//    });
+//});
